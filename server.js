780a4d6f4d79d3ea9425bab6b7328ba5806e48d7
heathercoraje <yhk7830@hotmail.com>
2017-07-28 16:20:39 +0300

    error

--- server.js
+++ server.js
@@ -11,23 +11,43 @@ server.listen(3000, function() {
 
 function handler(request, response) {
   var method = request.method;
-  console.log(method); //it returns get because we GET response by sending our url
-
-
+  //console.log(method); //it returns get because we GET response by sending our url
   var endpoint = request.url;
-  console.log(endpoint);
+  //console.log(endpoint);
 
   if (endpoint === "/") {
-    response.writeHead(200, {
-      "Content-Type": "text/html"
-    }); //using writehead method to check status code
-    // then let it know we will send text in body
-    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+    response.writeHead(200,
+      "Content-Type: text/html"
+    ); //using writehead method to check status code
+    fs.readFile(__dirname + "/public/index.html", function(error, file) {
       if (error) {
         console.log(error);
-        return;
+        return
+      } else { // then let it know we will send text in body
+        response.end(file);
+
       }
-      response.end(file);
     });
+
+  } else {
+    const extension = endpoint.split('.')[1];
+    const extensionType = {
+      html: 'text/html',
+      css: 'text/css',
+      jpg: 'image/jpg'
+    }
+    const filePath = path.join(__dirname, '..', endpoint)
+    fs.readFile(filePath, function(error, file) {
+      if (error) {
+        console.log(error);
+        return
+      } else {
+        response.writeHead(200, `Content-Type: ${type[extension]}`);
+        //response.writeHead(200, `Content-Type:${extensionType[extension]}`);
+
+        response.end(file);
+      }
+    });
+
   }
 }
