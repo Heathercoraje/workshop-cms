565c104b639635a90e6f8d9c80994ce6dbacbeff
heathercoraje <yhk7830@hotmail.com>
2018-01-12 18:20:06 +0900

    require fs module and send file as response

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 var http = require('http');// to have access to modules
 var server = http.createServer(handler); // handler function is called when request event happens
-var message = 'I am happy to redo a node girl project';
+var fs = require('fs');
+var node = 'Programming is hard, I know';
+var girls = 'Challenge fucking accepted !';
 server.listen(3000, function () {
   console.log('server is listening on port 3000, Ready to accept request');
 });
+//
+// function handler (request, response) {
+//   var endpoint = request.url;
+//   var message = '';
+//
+//   if (endpoint === '/node') {
+//     message = node;
+//   }
+//   if (endpoint === '/girls') {
+//     message = girls;
+//   }
+//
+//   response.writeHead(200, {'Content-Type': 'text/html'});
+//   response.write(message);
+//   response.end(); // finish response
+// }
 
 function handler (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(message);
-  response.end(); // finish response
+  var endpoint = request.url;
+  if (endpoint === '/') {
+    response.writeHead(200, {'Content-Type': 'text/html'}); // information of response I am sending
+
+    fs.readFile(__dirname + '/public/index.html', function (error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  }
 }
