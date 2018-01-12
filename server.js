2d5c3f78dc259bff62275cf5dc333ad2e3c34225
heathercoraje <yhk7830@hotmail.com>
2018-01-12 20:04:40 +0900

    create generic endpoint and router

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');// to have access to modules
 var server = http.createServer(handler); // handler function is called when request event happens
 var fs = require('fs');
+var path = require('path');
+
 var node = 'Programming is hard, I know';
 var girls = 'Challenge fucking accepted !';
 server.listen(3000, function () {
@@ -24,7 +26,8 @@ server.listen(3000, function () {
 // }
 
 function handler (request, response) {
-  var endpoint = request.url;
+  const endpoint = request.url;
+  // first request
   if (endpoint === '/') {
     response.writeHead(200, {'Content-Type': 'text/html'}); // information of response I am sending
 
@@ -34,6 +37,25 @@ function handler (request, response) {
         return;
       }
       response.end(file);
+    });
+  } else {
+    const extension = request.url.split('.')[1];
+    const extensionType = {
+      html: 'text/html',
+      css: 'text/css',
+      jpg: 'image/jpg'
+    };
+    const filePath = path.join(__dirname, 'public', endpoint);
+    console.log(filePath);
+
+    fs.readFile(filePath, function (error, file) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.writeHead(200, {'Content-Type': `${extensionType[extension]}`});
+        response.end(file);
+        console.log('yeah!');
+      }
     });
   }
 }
