58380e855fb0398cb96ff176d7911e28eb87c12b
heathercoraje <yhk7830@hotmail.com>
2018-01-12 17:42:51 +0900

    send response to the first request

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var http = require('http');// to have access to modules
-var server = http.createServer();
+var server = http.createServer(handler); // handler function is called when request event happens
+var message = 'I am happy to redo a node girl project';
+server.listen(3000, function () {
+  console.log('server is listening on port 3000, Ready to accept request');
+});
 
-server.listen(3000, function(){
-  console.log("server is listening on port 3000, Ready to accept request");
-})
+function handler (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(message);
+  response.end(); // finish response
+}
