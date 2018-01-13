66d857615c8047dcf6647fbf1f3638e58d457577
heathercoraje <yhk7830@hotmail.com>
2018-01-13 18:45:43 +0900

    get ready for the cms project

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');// to have access to modules
-var handlers = require('./src/handlers.js');
-
-var server = http.createServer(handlers); // handler function is called when request event happens
-
-server.listen(3000, function () {
-  console.log('server is listening on port 3000, Ready to accept request');
-});
